3b439946cb9027499eea93efdd1ffaac93f1ca10
Egioooo <134957932+SergDeved@users.noreply.github.com>
2023-06-09 15:03:03 +0200

    Add favicon and title

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,22 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      "weather-primary" : "#00668A",
-      "weather-secondary" : "#004E71"
-
+      colors: {
+        "weather-primary": "#00668A",
+        "weather-secondary": "#004E71"
+      },
+      fontFamily: {
+        Roboto: ["Roboto, sans-serif"]
+      },
+      container: {
+        padding: "2rem",
+        center: true,
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+      }
     },
-    fontFamily:{
-      Roboto: ["Roboto, sans-serif"]
-    },
-    container:{
-      padding: "2rem",
-      center: true,
-    },
-    screens: {
-      sm:"640px",
-      md:"768px",
-    }
   },
 
   plugins: [],
